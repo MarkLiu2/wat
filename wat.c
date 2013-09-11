8846535214ccc466b16e448f97285c2aa4222ae2
luizguilhermecm <luizgui@gmail.com>
2013-09-11 09:56:25 -0300

    WavHeader struct added

--- wat.c
+++ wat.c
@@ -1,4 +1,5 @@
 #include "wat.h"
+#include "string.h"
 
 WavHeader * wav_header = NULL;
 
@@ -17,6 +18,7 @@ int read_header_file(char *file_name)
         buffer = (unsigned char *) malloc(sizeof(unsigned char *) * HEADER_SIZE);
         if(buffer == NULL){
                 printf("\nMemory Error");
+                return -1;
         }
 
         ret = fread(buffer, sizeof(unsigned char *), HEADER_SIZE, f);
@@ -25,10 +27,44 @@ int read_header_file(char *file_name)
 
         if(ret < HEADER_SIZE){
                 printf("\nSomething is wrong with fread");
+                return -1;
         }
 
-        wav_header->chunk_size = 1234;
+        strncpy(wav_header->chunk_id, (char *)buffer, 4);
+        memcpy(&wav_header->chunk_size, buffer + 4, 4);
+        strncpy(wav_header->format, (char *)buffer + 8, 4);
 
+        strncpy(wav_header->subchunk1_id, (char *)buffer + 12, 4);
+        memcpy(&wav_header->subchunk1_size, buffer + 16, 4);
+        memcpy(&wav_header->audio_format, buffer + 20, 2);
+        memcpy(&wav_header->num_channels, buffer + 22, 2);
+        memcpy(&wav_header->sample_rate, buffer + 24, 4);
+        memcpy(&wav_header->byte_rate, buffer + 28, 4);
+        memcpy(&wav_header->block_align, buffer + 32, 2);
+        memcpy(&wav_header->bits_per_sample, buffer + 34, 2);
+
+        strncpy(wav_header->subchunk2_id, (char *)buffer + 36, 4);
+        memcpy(&wav_header->subchunk2_size, buffer + 40, 4);
+
+
+        printf("\nHeader of wav file:\n");
+        printf("\nchunk_id => %s", wav_header->chunk_id);
+        printf("\nchunk_size => %d", wav_header->chunk_size);
+        printf("\nformat => %s", wav_header->format);
+
+        printf("\n\nSubChunk 1:\n");
+        printf("\nsubchunk1_id => %s", wav_header->subchunk1_id);
+        printf("\nsubchunk1_size => %d", wav_header->subchunk1_size);
+        printf("\naudio_format => %d", wav_header->audio_format);
+        printf("\nnum_channels => %d", wav_header->num_channels);
+        printf("\nsample_rate => %d", wav_header->sample_rate);
+        printf("\nbyte_rate => %d", wav_header->byte_rate);
+        printf("\nblock_align => %d", wav_header->block_align);
+        printf("\nbits_per_sample => %d", wav_header->bits_per_sample);
+
+        printf("\n\nSubChunk 2:\n");
+        printf("\nsubchunk2_id => %s", wav_header->subchunk2_id);
+        printf("\nsubchunk2_size => %d", wav_header->subchunk2_size);
         return 1;
 }
 
@@ -40,13 +76,12 @@ int main(int argc, char *argv[])
         unsigned char * buffer;
         size_t result;
 
+        wav_header = malloc(sizeof(WavHeader));
+
         read_header_file("toque.wav");
 
+        /*
         pFile = fopen("toque.wav", "r");
-        if (pFile == NULL){
-                printf("\nFile error\n");
-        }
-
         fseek(pFile, 0, SEEK_END);
         file_size = ftell(pFile); 
         rewind(pFile);
@@ -64,8 +99,6 @@ int main(int argc, char *argv[])
 
         int i;
         printf("\nRIFF chunk descriptor (ASC) => ");
-        for (i = 0; i < 4; i++){
-        }
                  printf("%s", wav_header->chunk_id);
 
         fseek(pFile, 4, SEEK_SET);
@@ -154,7 +187,6 @@ int main(int argc, char *argv[])
 
         printf("\n\v DATA \v");
 
-
         for(i = 44; i < 500; i++){
                 if(i % 4 == 0)
                         printf("\n");
@@ -162,7 +194,7 @@ int main(int argc, char *argv[])
                         printf(".");
                 printf(" %02X ", buffer[i]);
         }
-
+*/
 
 
 
--- wat.h
+++ wat.h
@@ -7,20 +7,33 @@
 #define HEADER_SIZE 44
 
 typedef struct WavHeader{
-        char *chunk_id;
+        /* 4 bytes (0 - 4) */
+        char chunk_id[4];
+        /* 4 bytes (4 - 8) */
         int chunk_size;
+        /* 4 bytes (8 - 12) */
         char format[4];
 
+        /* 4 bytes (12 - 16) */
         char subchunk1_id[4];
+        /* 4 bytes (16 - 20) */
         int subchunk1_size;
+        /* 2 bytes (20 - 22) */
         int audio_format;
+        /* 2 bytes (22 - 24) */
         int num_channels;
+        /* 4 bytes (24 - 28) */
         int sample_rate;
+        /* 4 bytes (28 - 32) */
         int byte_rate;
+        /* 2 bytes (32 - 34) */
         int block_align;
+        /* 2 bytes (34 - 36) */
         int bits_per_sample;
 
+        /* 4 bytes (36 - 40) */
         char subchunk2_id[4];
+        /* 4 bytes (40 - 44) */
         int subchunk2_size;
 
 } WavHeader;
